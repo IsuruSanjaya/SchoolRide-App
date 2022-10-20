556adc51bebf8652d4bff2b8c849ce1b0943c62f
IsuruSanjaya <isurusanjayaa@gmail.com>
2022-10-20 21:57:49 +0530

    Fix all

--- app/src/main/java/com/ex/schoolride/Login.java
+++ app/src/main/java/com/ex/schoolride/Login.java
@@ -1,28 +1,39 @@
 package com.ex.schoolride;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class Login extends AppCompatActivity {
     TextInputEditText etLoginEmail;
     TextInputEditText etLoginPassword;
     TextView tvRegisterHere;
     Button btnLogin;
+    boolean valid = true;
+
 
     FirebaseAuth mAuth;
+    FirebaseFirestore fStore;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,37 +45,138 @@ public class Login extends AppCompatActivity {
         btnLogin = findViewById(R.id.btnLogin);
 
         mAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
 
-        btnLogin.setOnClickListener(view -> {
-            loginUser();
-        });
+
+//        btnLogin.setOnClickListener(view -> {
+//            loginUser();
+//        });
         tvRegisterHere.setOnClickListener(view ->{
             startActivity(new Intent(Login.this, Register.class));
         });
+
+    btnLogin.setOnClickListener((new View.OnClickListener() {
+        @Override
+        public void onClick (View view){
+        checkField(etLoginEmail);
+        checkField(etLoginPassword);
+        if (valid) {
+            mAuth.signInWithEmailAndPassword(etLoginEmail.getText().toString().trim(), etLoginPassword.getText().toString().trim()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                @Override
+                public void onSuccess(AuthResult authResult) {
+                    Toast.makeText(Login.this, "Logged success", Toast.LENGTH_SHORT).show();
+                    checkUserAccessLevel(authResult.getUser().getUid());
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+
+                    Toast.makeText(Login.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+
+            });
+        }
+    }
+    }));
+
     }
 
-    private void loginUser(){
-        String email = etLoginEmail.getText().toString();
-        String password = etLoginPassword.getText().toString();
 
-        if (TextUtils.isEmpty(email)){
-            etLoginEmail.setError("Email cannot be empty");
-            etLoginEmail.requestFocus();
-        }else if (TextUtils.isEmpty(password)){
-            etLoginPassword.setError("Password cannot be empty");
-            etLoginPassword.requestFocus();
-        }else{
-            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+    public boolean checkField(EditText textField){
+
+        if(textField.getText().toString().isEmpty()){
+            textField.setError("Error");
+            valid = false;
+        }else {
+            valid = true;
+        }
+
+        return valid;
+    }
+
+
+
+
+    //    private void loginUser(){
+//        String email = etLoginEmail.getText().toString();
+//        String password = etLoginPassword.getText().toString();
+//
+//        if (TextUtils.isEmpty(email)){
+//            etLoginEmail.setError("Email cannot be empty");
+//            etLoginEmail.requestFocus();
+//        }else if (TextUtils.isEmpty(password)){
+//            etLoginPassword.setError("Password cannot be empty");
+//            etLoginPassword.requestFocus();
+//        }else{
+//            mAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+//                @Override
+//                public void onComplete(@NonNull Task<AuthResult> task) {
+//                    if (task.isSuccessful()){
+//                        Toast.makeText(Login.this, "User logged in successfully", Toast.LENGTH_SHORT).show();
+//                        startActivity(new Intent(Login.this, MainActivity.class));
+//                    }else{
+//                        Toast.makeText(Login.this, "Log in Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+//                    }
+//                }
+//            });
+//        }
+//    }
+    public void checkUserAccessLevel(String uid){
+        DocumentReference df =fStore.collection("Users").document(uid);
+        df.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                Log.d(TAG, "Onsuccess"+ documentSnapshot.getData());
+
+                //IDENTIFY THE USER ACCESS
+
+                if(documentSnapshot.getString("isTeacher") !=null){
+                    //user admin
+
+                    startActivity(new Intent(getApplicationContext(),DriverHome.class));
+                    finish();
+
+                }
+
+                if(documentSnapshot.getString("isStudent") !=null){
+                    startActivity(new Intent (getApplicationContext(),StudentHome.class));
+                    finish();
+
+                }
+
+
+            }
+        });
+    }
+    protected   void onStart(){
+        super.onStart();
+        if(FirebaseAuth.getInstance().getCurrentUser() !=   null){
+            DocumentReference df = FirebaseFirestore.getInstance().collection("Users").document(FirebaseAuth.getInstance().getCurrentUser().getUid());
+            df.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
                 @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-                    if (task.isSuccessful()){
-                        Toast.makeText(Login.this, "User logged in successfully", Toast.LENGTH_SHORT).show();
-                        startActivity(new Intent(Login.this, MainActivity.class));
-                    }else{
-                        Toast.makeText(Login.this, "Log in Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                public void onSuccess(DocumentSnapshot documentSnapshot) {
+                    if(documentSnapshot.getString("isTeacher") !=null){
+                        startActivity(new Intent(getApplicationContext(),DriverHome.class));
+                        finish();
+
                     }
+                    if(documentSnapshot.getString("isStudent") !=null){
+                        startActivity(new Intent(getApplicationContext(),StudentHome.class));
+                        finish();
+                    }
+
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    FirebaseAuth.getInstance().signOut();
+                    startActivity(new Intent(getApplicationContext(),Login.class));
+
                 }
             });
+
+
+
         }
     }
 }
--- app/src/main/java/com/ex/schoolride/Register.java
+++ app/src/main/java/com/ex/schoolride/Register.java
@@ -1,71 +1,162 @@
 package com.ex.schoolride;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.text.TextUtils;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
 
-    TextInputEditText etRegEmail;
-    TextInputEditText etRegPassword;
-    TextView tvLoginHere;
-    Button btnRegister;
+    EditText fullName, email, password, phone;
+    Button registerBtn, goToLogin;
+    boolean valid = true;
+    FirebaseAuth fAuth;
+    FirebaseFirestore fStore;
+    CheckBox isTeacherBox,isStudentBox;
 
-     FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        etRegEmail = findViewById(R.id.etRegEmail);
-        etRegPassword = findViewById(R.id.etRegPass);
-        tvLoginHere = findViewById(R.id.tvLoginHere);
-        btnRegister = findViewById(R.id.btnRegister);
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
+        fullName = findViewById(R.id.registerName);
+        email = findViewById(R.id.registerEmail);
+        password = findViewById(R.id.registerPassword);
+        phone = findViewById(R.id.registerPhone);
+        registerBtn = findViewById(R.id.registerBtn);
+        goToLogin = findViewById(R.id.gotoLogin);
 
-        mAuth = FirebaseAuth.getInstance();
+        isTeacherBox = findViewById(R.id.isTeacher);
+        isStudentBox = findViewById(R.id.isStudent);
 
-        btnRegister.setOnClickListener(view ->{
-            createUser();
-        });
 
-        tvLoginHere.setOnClickListener(view ->{
-            startActivity(new Intent(Register.this, Login.class));
-        });
-    }
 
-    private void createUser(){
-        String email = etRegEmail.getText().toString();
-        String password = etRegPassword.getText().toString();
-
-        if (TextUtils.isEmpty(email)){
-            etRegEmail.setError("Email cannot be empty");
-            etRegEmail.requestFocus();
-        }else if (TextUtils.isEmpty(password)){
-            etRegPassword.setError("Password cannot be empty");
-            etRegPassword.requestFocus();
-        }else{
-            mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task) {
-                    if (task.isSuccessful()){
-                        Toast.makeText(Register.this, "User registered successfully", Toast.LENGTH_SHORT).show();
-                        startActivity(new Intent(Register.this, Login.class));
-                    }else{
-                        Toast.makeText(Register.this, "Registration Error: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
-                    }
+        //check one boxes login
+        isStudentBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if(compoundButton.isChecked()){
+                    isTeacherBox.setChecked(false);
                 }
-            });
-        }
+            }
+        });
+
+        isTeacherBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                if(compoundButton.isChecked()){
+                    isStudentBox.setChecked(false);
+                }
+            }
+        });
+
+
+
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                checkField(fullName);
+                checkField(email);
+                checkField(password);
+                checkField(phone);
+
+                //checkbox validation
+                if(!(isTeacherBox.isChecked() || isStudentBox.isChecked())){
+
+                    Toast.makeText(Register.this,"Select the account type",Toast.LENGTH_SHORT).show();
+                    return;
+
+
+
+                }
+
+                if (valid) {
+
+                    //start user registrtaion
+                    fAuth.createUserWithEmailAndPassword(email.getText().toString(),password.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                        @Override
+                        public void onSuccess(AuthResult authResult) {
+                            FirebaseUser user = fAuth.getCurrentUser();
+                            Toast.makeText(Register.this, "Account created", Toast.LENGTH_SHORT).show();
+                            DocumentReference df = fStore.collection("Users").document(user.getUid());
+                            Map<String,Object> userInfo = new HashMap<>();
+                            userInfo.put("FullName", fullName.getText().toString());
+                            userInfo.put("UserEmail", email.getText().toString());
+
+                            userInfo.put("PhoneNumber", phone.getText().toString());
+
+                            //specify
+                            if(isTeacherBox.isChecked()){
+                                userInfo.put("isDriver", "1");
+
+                            }
+                            if(isStudentBox.isChecked()){
+                                userInfo.put("isStudent", 1);
+
+                            }
+
+                            df.set(userInfo);
+                            if(isTeacherBox.isChecked()){
+                                startActivity(new Intent(getApplicationContext(), DriverHome.class));
+                                finish();
+
+                            }
+                            if(isStudentBox.isChecked()){
+                                startActivity(new Intent(getApplicationContext(),StudentHome.class));
+                                finish();
+
+                            }
+
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(Register.this, "Failed to create a account", Toast.LENGTH_SHORT).show();
+
+                        }
+                    });
+
+                }
+            }
+        });
+        goToLogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), Login.class));
+            }
+        });
     }
+
+    public boolean checkField(EditText textField) {
+        if (textField.getText().toString().isEmpty()) {
+            textField.setError("Error");
+            valid = false;
+        } else {
+            valid = true;
+        }
+
+        return valid;
+    }
+
 }
